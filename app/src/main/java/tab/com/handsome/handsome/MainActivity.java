2e6155ccfd41cd0d7f53c12b94b8a743bf0774bb
mzsjlwl <2855240636@qq.com>
2016-01-14 20:24:56 +0800

    yj

--- app/src/main/java/tab/com/handsome/handsome/MainActivity.java
+++ app/src/main/java/tab/com/handsome/handsome/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        jjjjjjjjjjjjjj
+        //kkkkkkkkk
         setContentView(R.layout.activity_main);
     }
 
