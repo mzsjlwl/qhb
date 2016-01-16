182515ee822cf0de4252b42ccbbd24b362eb3a75
mzsjlwl <2855240636@qq.com>
2016-01-16 14:10:49 +0800

    yj

--- app/src/main/java/com/handsome/qhb/ui/fragment/CycleFragment.java
+++ app/src/main/java/com/handsome/qhb/ui/fragment/CycleFragment.java
@@ -7,14 +7,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.handsome.qhb.application.ApplicationManager;
+
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smackx.muc.HostedRoom;
+import org.jivesoftware.smackx.muc.MultiUserChat;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import tab.com.handsome.handsome.R;
 
 public class CycleFragment extends Fragment {
 
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		
-		View view = inflater.inflate(R.layout.fragment_cycle, container, false);
-		return view;
-	}
+    private View view;
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_cycle, container, false);
+
+        try {
+            XMPPConnection connection = ApplicationManager.getXMPPConnection(this.getActivity());
+            Collection<HostedRoom> hostrooms = null;
+            List<HostedRoom> roominfos = new ArrayList<HostedRoom>();
+            hostrooms = MultiUserChat.getHostedRooms(connection,
+                    connection.getServiceName());
+            for (HostedRoom entry : hostrooms) {
+                roominfos.add(entry);
+                System.out.println("名字：" + entry.getName() + " - ID:" + entry.getJid());
+            }
+            System.out.println(roominfos.size()+"");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return view;
+    }
 }
--- app/src/main/java/com/handsome/qhb/ui/fragment/MeFragment.java
+++ app/src/main/java/com/handsome/qhb/ui/fragment/MeFragment.java
@@ -2,20 +2,59 @@ package com.handsome.qhb.ui.fragment;
 
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.handsome.qhb.application.ApplicationManager;
+import com.handsome.qhb.ui.activity.LoginActivity;
+
+import org.jivesoftware.smack.XMPPConnection;
 
 import tab.com.handsome.handsome.R;
 
-public class MeFragment extends Fragment {
+public class MeFragment extends Fragment implements View.OnClickListener {
 
+    private Button bt_loginOut;
+    private View view;
 
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_me, container, false);
-		return view;
-	}
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_me, container, false);
+        initViews();
+        return view;
+    }
 
+    private void initViews() {
+        bt_loginOut = (Button) view.findViewById(R.id.id_bt_loginOut);
+        bt_loginOut.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.id_bt_loginOut:
+                loginOut(ApplicationManager.getXMPPConnection(this.getActivity()));
+                Intent intent = new Intent(this.getActivity(), LoginActivity.class);
+                startActivity(intent);
+                this.getActivity().finish();
+                break;
+        }
+    }
+
+    /**
+     * 退出登录
+     * @param connection
+     * @return
+     */
+    private boolean loginOut(XMPPConnection connection) {
+        try{
+            connection.disconnect();
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
 }
