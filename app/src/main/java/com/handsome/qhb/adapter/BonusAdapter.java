a01272fbd934381757dc0165b92e0914294b4d3d
zhang <295253797@qq.com>
2016-03-29 13:13:04 +0800

    zhangsx 2016/3/29 13:12

--- app/src/main/java/com/handsome/qhb/adapter/BonusAdapter.java
+++ app/src/main/java/com/handsome/qhb/adapter/BonusAdapter.java
@@ -27,15 +27,9 @@ public class BonusAdapter extends CommonAdapter<RandomBonus> {
     }
     @Override
     public void convert(int position, ViewHolder holder, ListView listView, RandomBonus randomBonus) {
-//        user = gson.fromJson(randomBonus.getUser(),User.class);
         user = randomBonus.getUser();
         holder.setImage(R.id.iv_user_photo, user.getPhoto());
-        if(user.getUid()==0){
-            holder.setText(R.id.tv_user_nackname,"系统的红包");
-        }else{
-            holder.setText(R.id.tv_user_nackname,user.getNackname()+"的红包");
-        }
-
+        holder.setText(R.id.tv_user_nackname,user.getNackname());
         holder.setText(R.id.tv_time,randomBonus.getTime());
         holder.setText(R.id.tv_bonus, String.valueOf(randomBonus.getBonus()));
     }
